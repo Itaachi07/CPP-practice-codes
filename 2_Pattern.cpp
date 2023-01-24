cf148eb9384cb60090e44d5b90aa18490d88039f
D4_62758_PratikSatpute <pratiksatpute17@gmail.com>
2023-01-24 22:12:20 +0530

    patterns cpp

---
+++ 2_Pattern.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace  std;
+int main() 
+{
+    int rows, columns;
+    cout<<"Enter Number of rows"<<endl;
+    cin>>rows;
+
+    cout<<"Enter Number of columns"<<endl;
+    cin>>columns;
+
+    for (int i = 0; i <rows ; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+        
+    }
+    
+    return 0;
+}
