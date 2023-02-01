0c5cacad8d5896177fe4da167d0de45c42336ac7
D4_62758_PratikSatpute <pratiksatpute17@gmail.com>
2023-02-01 22:50:08 +0530

    arithmatic operations of variables with diff data types

---
+++ AdditionMixedType.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<iomanip>
+
+using namespace std;
+ int main()
+ {
+    int  m1 =10, m2= 15, m3=5;
+    double d1=3.5, d2 =6.7;
+
+    cout<< "Addition "<< m1+m2<< endl;
+    cout<< "Addition "<< m1+d2<< endl;
+    cout<< "Subtraction "<< m1-d2<< endl;
+    cout<< "Subtraction "<< d1-d2<< endl;
+    cout<< "Multi "<< d1*d2<< endl;
+    cout<< "Multi "<< m3*d2<< endl;
+    cout<< "Divison "<< m3/d2<< endl;
+    cout<< "Divison "<< m2/d1<< endl;
+
+ }
